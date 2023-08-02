68040e4f191262e4bac2c8ec5eac1e1f42da8d82
yamatosuzuki <yamatomatomatomatomato@gmail.com>
2023-08-02 15:01:37 +0900

    first commit

---
+++ ColorSensorCentral/ColorSensorStubApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorSensorStubApp.swift
+//  ColorSensorStub
+//
+//  Created by ysuzuki on 2023/08/01.
+//
+
+import SwiftUI
+
+@main
+struct ColorSensorStubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ColorSensorCentral/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  CorebluetoothBLESwiftUI
+//
+//  Created by cmStudent on 2023/01/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        NavigationStack {
+            VStack {
+                NavigationLink(destination: CentralView()) {
+                    Text("Central")
+                }
+                .buttonStyle(.borderedProminent)
+                .padding()
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
